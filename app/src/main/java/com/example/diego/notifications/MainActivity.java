12eee8a014852931587bfa77445403813a469ed4
diego <diego@zero.halloween.town>
2015-11-10 22:11:10 -0200

    Criado evento on click de um jeito um pouco diferente para que voce veja que dá pra fazer a mesma coisa de jeitos diferentes
    	Criado notificações, já começou a notificar com algo
    porém ainda precisa ser melhorado e adicionado um intent
    para ficar mais legal
    
    	modified:   app/src/main/java/com/example/diego/notifications/MainActivity.java

--- app/src/main/java/com/example/diego/notifications/MainActivity.java
+++ app/src/main/java/com/example/diego/notifications/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.diego.notifications;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    NotificationManager nManager;
+    int notificationId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_notificatoion = (Button) findViewById(R.id.btn_notificacao);
+        btn_notificatoion.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        notification();
+    }
+
+    private void notification() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.ic_stat_name);
+        mBuilder.setContentTitle(getResources().getString(R.string.ntf_titulo));
+        mBuilder.setContentText(getResources().getString(R.string.ntf_texto) + ": " + notificationId);
+
+        nManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nManager.notify(notificationId, mBuilder.build());
+        notificationId += 1;
     }
 }
