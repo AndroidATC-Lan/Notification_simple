30a5763adaba2d9e425c3075148d3fc81ebdc3a1
diego <diego@zero.halloween.town>
2015-11-10 22:26:15 -0200

    Adicionando contador 	Adicionando uma nova activity 	Adicionando contadores para ver qts vezes a notificação foi feita, porém para evitar muitas notificações, você pode verificar "já tem notificação" ai não aparece uma nova, mas muda o "style" da notificação para um novo número =D
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/example/diego/notifications/Destino.java
    	modified:   app/src/main/java/com/example/diego/notifications/MainActivity.java
    	new file:   app/src/main/res/drawable/potato.jpeg
    	new file:   app/src/main/res/layout/activity_destino.xml
    	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/diego/notifications/MainActivity.java
+++ app/src/main/java/com/example/diego/notifications/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.diego.notifications;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,10 +32,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void notification() {
+        Log.e("Log", "Criando o builder p/ notification");
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setSmallIcon(R.drawable.ic_stat_name);
         mBuilder.setContentTitle(getResources().getString(R.string.ntf_titulo));
         mBuilder.setContentText(getResources().getString(R.string.ntf_texto) + ": " + notificationId);
+
+        Log.e("Log", "Criando o intent para o destino da notification");
+        Intent resultIntent = new Intent(this, Destino.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Destino.class);
+        stackBuilder.addNextIntent(resultIntent);
+        Log.e("Log", "Atualiza a flag da notification");
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Log.e("Log", "Passando o intent para o builder");
+        mBuilder.setContentIntent(resultPendingIntent);
 
         nManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         nManager.notify(notificationId, mBuilder.build());
